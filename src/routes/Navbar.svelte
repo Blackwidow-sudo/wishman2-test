3afc7c29586eba2fc6a87284d21a07797cd45acb
Merlin Cornehl <merlin.cornehl@web.de>
2022-12-05 22:34:02 +0100

    update Navbar.svelte

--- src/routes/Navbar.svelte
+++ src/routes/Navbar.svelte
@@ -16,39 +16,43 @@
 </script>
 
 <nav>
-	<ul>
-		{#each pages as { name, href }}
-			<li>
-				<a {href} class:active={$page.url.pathname === href}>{name}</a>
+	<ul class="flex flex-row bg-blue-300">
+		{#each pages as { name, href } (name)}
+			<li class="px-4 py-2 bg-blue-300 hover:bg-blue-400">
+				<a
+					{href}
+					class:active={$page.url.pathname === href}>
+					{name}
+				</a>
 			</li>
 		{/each}
 	</ul>
 </nav>
 
 <style lang="scss">
-	ul {
-		list-style-type: none;
-		display: flex;
-		flex-direction: row;
-		gap: 1rem;
+	// ul {
+	// 	list-style-type: none;
+	// 	display: flex;
+	// 	flex-direction: row;
+	// 	gap: 1rem;
 
-		background-color: #f39ab7;
-		padding: 0.5rem;
-		margin: 0;
+	// 	background-color: #f39ab7;
+	// 	padding: 0.5rem;
+	// 	margin: 0;
 
-		li {
-			background-color: #f5aac3;
-			padding: 0.5rem 1rem;
-		}
+	// 	li {
+	// 		background-color: #f5aac3;
+	// 		padding: 0.5rem 1rem;
+	// 	}
 
-		li:hover {
-			background-color: #c6889d;
-		}
+	// 	li:hover {
+	// 		background-color: #c6889d;
+	// 	}
 
-		a {
-			text-decoration: none;
-		}
-	}
+	// 	a {
+	// 		text-decoration: none;
+	// 	}
+	// }
 
 	.active {
 		text-decoration: underline;

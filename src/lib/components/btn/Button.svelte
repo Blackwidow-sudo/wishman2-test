78c3315dd276f6cc71b2c8aeea341360f8adf6e8
Merlin Cornehl <merlin.cornehl@web.de>
2022-12-05 22:31:52 +0100

    add Button.svelte

---
+++ src/lib/components/btn/Button.svelte
@@ -0,0 +1,14 @@
+<script lang="ts">
+	export let center: boolean = false
+	export let block: boolean = false
+</script>
+
+<button
+	type="button"
+	class="px-4 py-2 border bg-gray-300 hover:bg-gray-400 rounded-md"
+	class:mx-auto={center}
+	class:block
+	{...$$restProps}
+	on:click>
+	<slot />
+</button>

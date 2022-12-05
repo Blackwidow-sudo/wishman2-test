3bbbe0b6849f7adcb3ea0fd7b39ef0ab4a349556
Merlin Cornehl <merlin.cornehl@web.de>
2022-12-05 22:34:17 +0100

    update login page

--- src/routes/auth/login/+page.svelte
+++ src/routes/auth/login/+page.svelte
@@ -1,4 +1,8 @@
 <script lang="ts">
+	import SubmitButton from '$lib/components/btn/SubmitButton.svelte'
+	import EmailField from '$lib/components/form/EmailField.svelte'
+	import Form from '$lib/components/form/Form.svelte'
+	import PasswordField from '$lib/components/form/PasswordField.svelte'
 	import PageTransition from '$lib/components/PageTransition.svelte'
 
 	const formData = {
@@ -6,7 +10,9 @@
 		password: ''
 	}
 
-	function onSubmit() {}
+	function onSubmit() {
+		alert(`{ email: ${formData.email}, password: ${formData.password} }`)
+	}
 </script>
 
 <svelte:head>
@@ -14,9 +20,22 @@
 </svelte:head>
 
 <PageTransition>
-	<form on:submit|preventDefault={onSubmit}>
-		<input type="email" placeholder="Enter email" />
-		<input type="password" placeholder="Enter password" />
-		<input type="submit" value="Sign in" />
-	</form>
+	<div class="flex items-center justify-center">
+		<Form
+			title="Sign In"
+			on:submit={onSubmit}>
+			<svelte:fragment slot="fields">
+				<EmailField
+					id="email"
+					label="Email:"
+					placeholder="Enter email"
+					bind:value={formData.email} />
+				<PasswordField
+					id="password"
+					label="Password:"
+					placeholder="Enter password"
+					bind:value={formData.password} />
+			</svelte:fragment>
+		</Form>
+	</div>
 </PageTransition>

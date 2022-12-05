a9eab0965b3af63b2e1cf37055d6e4dde9910fd6
Merlin Cornehl <merlin.cornehl@web.de>
2022-12-05 22:33:19 +0100

    add EmailField.svelte & PasswordField.svelte

---
+++ src/lib/components/form/EmailField.svelte
@@ -0,0 +1,14 @@
+<script lang="ts">
+	export let id: string
+	export let label: string
+	export let value: string = ''
+	export let placeholder: string = ''
+</script>
+
+<label for={id}>{label}</label>
+<input
+	class="px-3 py-2 shadow border rounded-md leading-tight"
+	type="email"
+	{id}
+	{placeholder}
+	bind:value />
